612b4a09d5c21be37b0d74085a133635b3543dc7
codingk8 <codingk8@gmail.com>
2019-04-04 14:24:38 +0200

    add the js file

---
+++ scripts.js
@@ -0,0 +1,52 @@
+/* -----------------------------------------------------------------------
+  Made with code and care in Paris by Codingk8 - 2019 - Twitter: @codingk8
+
+  Project made for the freeCodeCamp curriculum, "Responsive Web Design" Certification
+  Project #4 - Technical Documentation
+
+  Title: Markdown up and running - An extended cheatsheet
+------------------------------------------------------------ */
+
+
+/* --------------------------------------------------------------------------------------- */
+/* --- Makes the "back to top" arrow appear after scroll and disappear when back up => --- */
+/* --------------------------------------------------------------------------------------- */
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 125 || document.documentElement.scrollTop > 125) {
+    document.getElementById("btticon").style.display = "block";
+  } else if (document.body.scrollBottom > 125 || document.documentElement.scrollBottom > 125) {
+    document.getElementById("btticon").style.display = "none";
+  } else {
+    document.getElementById("btticon").style.display = "none";
+  }
+}
+
+
+/* ---------------------------------------------------------- */
+/* --- jQuery Bounce effect on the "back to top" arrow => --- */
+/* ---------------------------------------------------------- */
+
+$(document).ready(function(){
+    $( "#btticon" ).on("mouseenter", function() {
+        $(this).effect( "bounce", { times: 3 }, "slow" );
+    })
+})
+
+
+/* ------------------------------------------ */
+/* --- "Light mode | Dark mode" effect => --- */
+/* ------------------------------------------ */
+
+function toggleDarkLight() {
+  let container = document.getElementById("container");
+  let currentClass = container.className;
+  container.className = currentClass == "dark" ? "light" : "dark";
+}
+
+
+/* ------------------ */
+/* End of js file ;o) */
+/* ------------------ */
